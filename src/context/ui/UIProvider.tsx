8a55f389c28f9fb7ca9e708419777f65ffaf3bbd
HuitziDomingo <huitzitzilidomingo@gmail.com>
2023-01-23 20:52:44 -0600

    context y reducer agregados

--- src/context/ui/UIProvider.tsx
+++ src/context/ui/UIProvider.tsx
@@ -1,16 +1,24 @@
-import { FC } from 'react'
-import { UIContext } from './UIContext'
+import { FC, useReducer } from 'react'
+import { UIContext, uiReducer } from './'
 
 export interface UIState {
     sidemenuOpen: boolean
 }
 
-const UI_INITIAL_STATEL: UIState = {
+interface Props {
+    children?: JSX.Element | JSX.Element[]
+}
+
+const UI_INITIAL_STATE: UIState = {
     sidemenuOpen: false
 }
 
-export const UIProvider: FC = () => {
+export const UIProvider: FC = ({ children }: Props) => {
+    const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE)
+
     return (
-        <div></div>
+        <UIContext.Provider value={{ sidemenuOpen: false }}>
+            {children}
+        </UIContext.Provider>
     )
 }
